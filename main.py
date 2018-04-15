c802d22183ff58e6c67d44da8c312c3f68fcf80c
kyle merfeld <kmerfeld1@gmail.com>
2018-04-15 17:10:14 -0500

    Implements simple get

---
+++ main.py
@@ -0,0 +1,67 @@
+""" This allows people to create/view posts"""
+from pprint import pprint
+from flask import Flask
+from flask_restful import Resource, Api
+from pymongo import MongoClient
+import configparser
+import dateutil
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+#GLOBALS
+DB_URL = ''
+DB_PORT = ''
+DB_CLIENT = MongoClient()
+
+class Post:
+    """Post objects we want to show off"""
+    user = ''
+    date = ''
+    body = ''
+    def __init__(self, user, date, body):
+        self.user = user
+        self.date = date
+        self.body = body
+
+    def toJson(self):
+        return json.dumps(self)
+
+
+
+class Feed(Resource):
+    """Get posts from a user"""
+
+    def get(self, user):
+        db = DB_CLIENT.test
+        posts  = db.testcollection.find({"User": user})
+
+        p = []
+        for i in posts:
+            p.append({'user': i['User'], 'date': i['date'], 'body': i['body']})
+
+        #status = str(serverStatusResult)
+        status = 'alls good'
+        return { 
+            'USER': user,
+            'result_count': p,
+        }
+
+api.add_resource(Feed, '/<user>')
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.sections()
+    config.sections()
+    config.read('config.ini')
+    DB_URL = config['Database']['url']
+    DB_PORT = config['Database']['port']
+    DB_CLIENT = MongoClient(DB_URL, int(DB_PORT)) 
+
+#post = {'User': 'bob', 'date': 'today', 'body': 'this is a post'}
+        #post_id = db.testcollection.insert_one(post).inserted_id
+        #print(post_id, 'this is the tihgn')
+
+
+    app.run(debug=True)
